8f51de3221538a7365268272bbeb04dbb25101cd
Omar Alvarez <omar.alvarez2006@gmail.com>
2015-06-27 20:39:04 +0100

    Export config module

--- config/config.js
+++ config/config.js
@@ -1,20 +1,20 @@
-{
-  "development": {
-    "dialect": "sqlite",
-    "storage": "./db/development.sqlite"
+module.exports = {
+  development: {
+    dialect: "sqlite",
+    storage: "./db/development.sqlite"
   },
-  "test": {
-    "username": process.env.DATABASE_MYSQL_USERNAME,
-    "password": process.env.DATABASE_MYSQL_PASSWORD,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: process.env.DATABASE_MYSQL_USERNAME,
+    password: process.env.DATABASE_MYSQL_PASSWORD,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql"
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql"
   }
 }
