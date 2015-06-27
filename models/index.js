1a5875a4311d3e79edeb09e6ab556e097cfaf9e9
Omar Alvarez <omar.alvarez2006@gmail.com>
2015-06-27 20:18:47 +0100

    Reference semaphore mysql environment variables right from the config.js file

--- models/index.js
+++ models/index.js
@@ -6,7 +6,6 @@ var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
-var config.password  = process.env.DATABASE_MYSQL_PASSWORD || config.password;
 var sequelize = new Sequelize(config.database, config.username, config.password, config);
 var db        = {};
 
