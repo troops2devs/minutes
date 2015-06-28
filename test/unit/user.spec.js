76bc455f597387126203de063ad1fb87d587bb10
John Sheedy <jtsheedy@yahoo.com>
2015-06-28 09:45:24 +0100

    add done function to user spec

--- test/unit/user.spec.js
+++ test/unit/user.spec.js
@@ -2,15 +2,16 @@
 
 var expect = require('chai').expect;
 
-describe('models/user', function () {
-  beforeEach(function () {
+describe('models/user', function() {
+  beforeEach(function() {
     this.User = require('../../models').User;
   });
 
-  describe('create', function () {
-    it('creates a user', function () {
-      return this.User.create({ username: 'mr_t' }).then(function (user) {
+  describe('create', function() {
+    it('creates a user', function(done) {
+      return this.User.create({ username: 'mr_t' }).then(function(user) {
         expect(user.username).to.equal('mr_t');
+        done();
       });
     });
   });
