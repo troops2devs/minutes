f34ac059165f69875aa057c8e59f34716484f8a4
Omar Alvarez <omar.alvarez2006@gmail.com>
2015-07-07 17:06:19 +0100

    Fix require path for users controller

--- api/api.js
+++ api/api.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 var router = express.Router();
-var usersController = require('../controllers/users');
+var usersController = require('../controllers/users.controller');
 var studentsController = require('../controllers/students');
 
 router.get('/', function(req, res) {
