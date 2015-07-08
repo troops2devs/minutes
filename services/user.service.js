104e18211e439ce8159f025ca5da396b4aa58963
Omar Alvarez <omar.alvarez2006@gmail.com>
2015-07-08 21:32:15 +0100

    Add controller and integration tests

--- services/user.service.js
+++ services/user.service.js
@@ -13,6 +13,28 @@ var userService = {
     userModel.findById(id).then(function(user) {
       cb(user);
     });
+  },
+
+  saveUser: function(userAttrs, cb) {
+    userModel.create(userAttrs).then(function(user) {
+      cb(user);
+    });
+  },
+
+  updateUser: function(userAttrs, cb) {
+    userModel.findById(userAttrs.id).then(function(user) {
+      user.updateAttributes(userAttrs).then(function(user) {
+        cb(user);
+      });
+    });
+  },
+
+  deleteUser: function(id, cb) {
+    userModel.findById(id).then(function(user) {
+      user.destroy().then(function(user) {
+        cb(user);
+      });
+    });
   }
 };
 
